646fb1e8653ad78b26cad79210ea9ada67281b43
anubhavprabhakar <anu.devjob@gmail.com>
2024-02-23 17:55:53 +0530

    feat: add display and buttons
    Make basic calculator structure (visual only) using HTML and CSS.
    A basic calculator includes display for user-input, buttons for numbers,
    operators, 'clear', '=' buttons.

--- script.js
+++ script.js
@@ -14,3 +14,29 @@ function divide(num1, num2){
     if(num2!==0) return num1 / num2
     else return 'Cannot divide by zero.'
 }
+
+const num1 = null;
+const num2 = null;
+const operator = null;
+
+function operate(operator, num1, num2){
+    let result = null;
+    switch(operator){
+        case '+':
+            result = add(num1, num2);
+            break;
+        case '-':
+            result = subtract(num1, num2);
+            break;
+        case '*':
+            result = multiply(num1, num2)
+            break;
+        case '/':
+            result = divide(num1, num2)
+            break;
+        default:
+            break;
+    }
+    console.log('result: ', result)
+}
+
